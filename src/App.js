20637957b9702219383f5019f5d113f2e2dc3104
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-03 16:08:52 -0500

    added error page with link to home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MovieList from './pages/MovieList'
 import MovieInfo from './pages/MovieInfo'
+import Error from './pages/Error'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={MovieList} />
             <Route exact path="/Movie/:movieID" component={MovieInfo} />
+            <Route component={Error} />
           </Switch>
         </Router>
       </>
