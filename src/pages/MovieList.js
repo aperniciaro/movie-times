c45a01e1d694e41b5d1380b4463c8c09d26f0f5a
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-03 14:30:56 -0500

    basic html layout for movielist page

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react'
 
 class MovieList extends Component {
   render() {
-    return <h1>Movie List</h1>
+    return (
+      <>
+        <header>
+          <h1>Now Showing</h1>
+        </header>
+        <section class="highlighted-movie">
+          <h2>Featuring</h2>
+          <p>Highlighted Movie</p>
+        </section>
+        <ul>
+          <li>Title</li>
+          <li>Title</li>
+          <li>Title</li>
+        </ul>
+      </>
+    )
   }
 }
 
