7828616a52d2d5e9250cdfe95dafa9656c7505fd
Anastasia Malissovas <anastasiamalissovas@gmail.com>
2019-03-03 19:58:43 -0500

    Merge and compilation resolved and site is now displayd and site displaying.

--- src/pages/MovieInfo.js
+++ src/pages/MovieInfo.js
@@ -1,81 +1,77 @@
 import React, { Component } from 'react'
-import MovieInfo from '../MovieInfo'
 import axios from 'axios'
 
 class MovieInfo extends Component {
   state = {
-    apiKey: '8d06228140322691066ef39ba52dfbb4'
-   // results: [],
-   // title: '',
-    //description: '',
-  //  releaseDate: '',
-  //  poster: '',
-   // id: ''
-    ///this is where state will go, move name, image link, etc
+    apiKey: '8d06228140322691066ef39ba52dfbb4',
+    results: [],
+    title: '',
+    description: '',
+    releaseDate: '',
+    poster: '',
+    id: ''
   }
 
   componentDidMount() {
-    const apiKey = Object.keys(this.state.id)
-    console.log(apiKey)
     axios
       .get(
-      `https://api.themoviedb.org/3/movie${
-         this.props.match.params.movieInfo
-       }&api_key=${
-         this.state.apiKey
-       }&append_to_response=images`
-    })
-      
+        `https://api.themoviedb.org/3/movie${
+          this.props.match.params.movieInfo
+        }&api_key=${this.state.apiKey}&append_to_response=images`
+      )
 
-    .then(resp => {
-      console.log({ resp })
+      .then(resp => {
+        console.log({ resp })
 
-      this.setState({
-        title: resp.data.original_title,
-        description: resp.data.overview,
-        releaseDate: resp.data.release_date,
-        poster: resp.data.poster_path
+        this.setState({
+          title: resp.data.original_title,
+          description: resp.data.overview,
+          releaseDate: resp.data.release_date,
+          poster: resp.data.poster_path
+        })
+        console.log(this.state)
       })
-      console.log(this.state)
-    })
-  this.getCast()
-}
+    this.getCast()
+  }
 
-getCast = () => {
-  axios
-    .get(
-      `https://api.themoviedb.org/3/movie/${
-        this.props.match.params.movie
-      }/credits?api_key=8d06228140322691066ef39ba52dfbb46`
+  getCast = () =>
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${
+          this.props.match.params.movie
+        }/credits?api_key=8d06228140322691066ef39ba52dfbb46`
+      )
+      .then(resp => {
+        console.log({ resp })
+
+        this.setState({
+          cast: resp.data.cast
+        })
+        console.log(this.state.cast[1].name)
+      })
+
+  render() {
+    return (
+      <div>
+        <figure>
+          <h1>{this.state.title}</h1>
+          <h1>{this.state.releaseDate}</h1>
+          <h3>{this.state.genre}</h3>
+          <img
+            src={`//https:image.tmdb.org/t/p/original${this.state.poster}`}
+            alt=""
+          />
+          <figcaption>{this.props.description}</figcaption>
+        </figure>
+        <ul>
+          {this.state.cast.map((cast, i) => {
+            console.log(cast)
+            return <li key={i}>{this.state.cast[i].name}</li>
+          })}
+        </ul>
+      </div>
     )
-    .then(resp => {
-      console.log({ resp })
-
-      this.setState({
-        cast: resp.data.cast
-      })
-      console.log(this.state.cast[1].name)
-    })
+  }
 }
 
-render() {
-  return (
-    <>
-      <MovieInfo
-        title={this.state.title}
-        description={this.state.description}
-        genre={this.state.genre}
-        releaseDate={this.state.releaseDate}
-        poster={`//https:image.tmdb.org/t/p/original${this.state.poster}`}
-        // cast={this.state.cast.name}
-      />
-      <ul>
-        {this.state.cast.map((cast, i) => {
-          console.log(cast)
-          return <li key={i}>{this.state.cast[i].name}</li>
-        })}
-      </ul>
-    </>
-  )
-}
 export default MovieInfo
